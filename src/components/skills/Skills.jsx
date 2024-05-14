45c25ecf94fb7aec1f62635c25e637a40d2af44c
Florian <68642839+FlorianPx@users.noreply.github.com>
2024-05-14 15:55:42 +0200

    feat: remove comments

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -29,35 +29,9 @@ const Cards = styled.div`
 `
 
 export const Skills = () => {
-  // const letters = [
-  //   { letter: 'C', delay: '0.13' },
-  //   { letter: 'o', delay: '0.12' },
-  //   { letter: 'm', delay: '0.11' },
-  //   { letter: 'p', delay: '0.1' },
-  //   { letter: 'é', delay: '0.09' },
-  //   { letter: 't', delay: '0.08' },
-  //   { letter: 'e', delay: '0.07' },
-  //   { letter: 'n', delay: '0.06' },
-  //   { letter: 'c', delay: '0.05' },
-  //   { letter: 'e', delay: '0.04' },
-  //   { letter: 's', delay: '0.03' },
-  // ]
-
   return (
     <Section>
-      <Title>
-        Compétences
-        {/* <span>
-          {letters.map((item, index) => (
-            <span
-              // eslint-disable-next-line react/no-array-index-key
-              key={`${item.letter}-${index}`}
-            >
-              {item.letter}
-            </span>
-          ))}
-        </span> */}
-      </Title>
+      <Title>Compétences</Title>
       <Cards>
         {cardArray.map((card, index) => (
           <Card card={card} key={`skill_${index + card.title}`} />
